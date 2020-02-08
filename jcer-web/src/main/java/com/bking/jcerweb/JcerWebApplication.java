3443b35f94c0d3666658f86b3f9bc2219f2bcf1c
B.K.KIM <ddiddikk@naver.com>
2020-02-09 01:36:29 +0900

    [2020-02-08] G-O!

---
+++ jcer-web/src/main/java/com/bking/jcerweb/JcerWebApplication.java
@@ -0,0 +1,13 @@
+package com.bking.jcerweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JcerWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JcerWebApplication.class, args);
+    }
+
+}
---
+++ jcer-web/src/test/java/com/bking/jcerweb/JcerWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bking.jcerweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JcerWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
